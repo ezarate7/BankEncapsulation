cb8264fc871c85bcc7f803619823d7cad138c406
Eddie Z <e.zarate7@hotmail.com>
2022-08-19 10:06:36 -0600

    initial commit

---
+++ BankEncapsulation/Program.cs
@@ -0,0 +1,8 @@
+﻿using BankEncapsulation;
+using System.Collections.Generic;
+
+var list = new List<string>();
+var account = new BankAccount();
+
+account.Deposit(1000);
+Console.WriteLine(account.GetBalance());
